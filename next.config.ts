48af26404c061e11c79840e00eb115981313fd0d
m.elkhamisy <m.elkhamisy@rakdao.com>
2026-04-09 15:02:49 +0400

    Update SALES_AGENT_ORIGIN in next.config.ts to point to the new gateway URL
    - Changed the SALES_AGENT_ORIGIN constant to the new gateway URL for improved connectivity in the application.
    
    This update ensures that the application interacts with the correct environment for sales agent functionalities.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
-const SALES_AGENT_ORIGIN = "https://sales-sandbox.innovationcity.com";
-
+const SALES_AGENT_ORIGIN =
+  "https://exp-gateway-impl-5eeid8.5i32o8.deu-c1.eu1.cloudhub.io";
+// exp-gateway-impl-5eeid8.5i32o8.deu-c1.eu1.cloudhub.io
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
